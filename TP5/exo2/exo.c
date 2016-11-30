a718aa07b24d821ad5b6dd2a768c77f72ac01d58
Wall-e <wallforfry@gmail.com>
2016-11-30 13:59:28 +0100

    TP5 : ajout rapport

--- TP5/exo2/exo.c
+++ TP5/exo2/exo.c
@@ -1,31 +1,24 @@
 #include <stdio.h>
 
-void affiche_bin(unsigned short int x)
+int main( )
 {
-	unsigned short int mask = 0b1000000000000000;
-	int i;
-	for (i=0; i<16; i++)
-	{
-		printf("%d", (mask & x)>>(15-i));
-		mask = mask>>1;
-	}
-}
+  unsigned short x = 0b0000111000101100;
 
-int main()
-{
-	unsigned short int x = 0b0000111000101100;
-	unsigned short int masque1 = 0b1111111100000000;
-	unsigned short int masque2 = 0b0000000011111111;
+  unsigned short poidsfort;
+  unsigned short masque1 = 0b1111111100000000;
 
-	unsigned short int poidsfort = (x & masque1) >> 8;
-	unsigned short int poidsfaible = (x & masque2);
+  int i = 0;
+  for(i = 0; i < 16; i++){
+    printf("%i : ", i);
+      if(x & masque1){
+        printf("1");
+      }
+      else {
+        printf("0");
+      }
+      printf("\n");
+      masque1 = masque1 >> 1;
+  }
 
-	printf("Poids Fort : %d = %x = ", poidsfort, poidsfort);
-	affiche_bin(poidsfort);
-
-	printf("\nPoids Faible : %d = %x = ", poidsfaible, poidsfaible);
-	affiche_bin(poidsfaible);
-
-  printf("\n");
-	return 0;
+  return 0 ;
 }
